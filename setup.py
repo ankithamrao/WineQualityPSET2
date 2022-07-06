d835dc9e144fa7e2c625d0fefc01435098fede60
ankithamrao <ankithamrao@gmail.com>
2022-07-06 14:28:35 +0530

    Creating project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building a ML app and deploying it on AWS',
+    author='ankitha',
+    license='MIT',
+)
